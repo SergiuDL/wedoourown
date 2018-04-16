d0d384c725e77835e0e1000754c0ffa8fe12d961
Sergiu <lascusergiudan@gmail.com>
2018-04-16 17:27:23 +0300

    commit 1

---
+++ src/main/java/com/tzacapaca/wedoourown/WedoourownApplication.java
@@ -0,0 +1,12 @@
+package com.tzacapaca.wedoourown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WedoourownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WedoourownApplication.class, args);
+	}
+}
