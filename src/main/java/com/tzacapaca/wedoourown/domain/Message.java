a8c1ad50c0ec3a12f8bd6fb68655e19d47efbe6d
ecarazan <ecaterina.carazan@gmail.com>
2018-04-16 18:46:52 +0300

    lombok added

--- src/main/java/com/tzacapaca/wedoourown/domain/Message.java
+++ src/main/java/com/tzacapaca/wedoourown/domain/Message.java
@@ -1,11 +1,13 @@
 package com.tzacapaca.wedoourown.domain;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
 
 @Document(collection = "messages")
+@Data
 public class Message {
     @Id
     private String  id;
