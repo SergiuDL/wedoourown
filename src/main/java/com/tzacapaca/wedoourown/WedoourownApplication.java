6ca352fa393f73f23f0baa7b03c2e7b52e09fc47
ecarazan <ecaterina.carazan@gmail.com>
2018-04-16 18:31:03 +0300

    mongo added

--- src/main/java/com/tzacapaca/wedoourown/WedoourownApplication.java
+++ src/main/java/com/tzacapaca/wedoourown/WedoourownApplication.java
@@ -2,10 +2,15 @@ package com.tzacapaca.wedoourown;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.cassandra.CassandraDataAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
+import org.springframework.data.cassandra.repository.config.EnableReactiveCassandraRepositories;
 
-@SpringBootApplication
+//@ComponentScan("com.tzacapaca.wedoourown")
+@SpringBootApplication()
+//@EnableCassandraRepositories
 public class WedoourownApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(WedoourownApplication.class, args);
 	}
